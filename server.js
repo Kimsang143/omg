7467b1b5cd05089a9d9deb1c9eee89a266797ca2
JimJim143 <30286387+Kimsang143@users.noreply.github.com>
2019-06-26 12:15:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+//app.get('/*', function(req, res) {
+///  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//});
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000);
