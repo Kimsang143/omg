344ded730bafaebc74c2b558ea18ce927ff26394
Kim Sang <kimsang@gmail.com>
2019-06-26 12:04:50 +0700

    first commit

---
+++ build/precache-manifest.b9d7da227c6105f4157b8634afba8477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2edd2d41f01b2dc6223b324cf628fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4fd865064090156143",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "ec09add4666c3a4eba2c",
+    "url": "/static/js/2.41b704ab.chunk.js"
+  },
+  {
+    "revision": "6d4fd865064090156143",
+    "url": "/static/js/main.374a7d1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
